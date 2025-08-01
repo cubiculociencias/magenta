51e4b81f4ca770313775ac54951c7c7a479a4fcf
cubiculociencias <piano.unam@gmail.com>
2025-08-01 00:25:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def transcribe():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port)
